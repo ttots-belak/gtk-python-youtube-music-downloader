e2a16b60ce6480b48313f27d63d12a469bb305d4
kaleb <xxxnullizationxxx@gmail.com>
2024-02-20 20:29:16 -0700

    removed unnecessary fields from index.py and added more mp3 tags to the download function

--- dl_handler.py
+++ dl_handler.py
@@ -1,48 +1,25 @@
 import gi
 from os import mkdir, system
 from pytube import YouTube 
-from pytube.exceptions import VideoUnavailable
 from pytube import Playlist 
 import ffmpeg
-from mutagen.mp3 import MP3  
-from mutagen.easyid3 import EasyID3
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
-def check_and_download(window, args):
-    user_input_needed = ""
-    user_input_correct = True
-    if "link" not in args:
-        user_input_correct = False
-        user_input_needed = user_input_needed + "link "
-    if "folder" not in args:
-        user_input_correct = False
-        user_input_needed = user_input_needed + "folder "
-    if not user_input_correct:
-        dialog = Gtk.MessageDialog(window, 0, Gtk.MessageType.ERROR,
-            Gtk.ButtonsType.OK, "ARR NARRR")
-        dialog.format_secondary_text(
-                "You are missing the following things: " + user_input_needed)
-        dialog.run()
-        dialog.destroy()
-        return 
-    try:
-        artist = args["artist"]
-    except:
-        artist = ""
-    try:
-        album = args["album"]
-    except:
-        album = ""
-
-    if args["is_album"] == True:
-        play = Playlist(args["link"]) 
-        play_folder = args["folder"] + "/" + play.title
-        mkdir(play_folder)
-        print(play_folder)
-        for link in play.video_urls:
-            dl_audio(window, link, args["ft"], play_folder, artist, album)
-    elif args["is_album"] == False:
-        dl_audio(window, args["link"], args["ft"], args["folder"], artist, album)
+def check_and_download(window, link, folder, filetype):
+    if link is None or folder is None or filetype is None:
+        error_message(window, "Required fields are missing!!!!!")
+        return
+    if "playlist" in link:
+        play = Playlist(link) 
+        play_folder = folder + "/" + play.title
+        trackno = 0
+        for vid in play.video_urls:
+            dl_audio(window, vid, filetype, play_folder, play.title, trackno, play.length)
+            trackno = trackno + 1
+    elif "watch" in link:
+        dl_audio(window, link, filetype, folder)
+    else:
+        error_message(window, "Couldn't parse link!")
 
 def error_message(window, message):
     
@@ -53,7 +30,7 @@ def error_message(window, message):
     dialog.run()
     dialog.destroy()
 
-def dl_message(window, message):
+def info_message(window, message):
     
     dialog = Gtk.MessageDialog(window, 0, Gtk.MessageType.INFO,
                                Gtk.ButtonsType.OK, "YO!")
@@ -61,7 +38,7 @@ def dl_message(window, message):
             message)
     dialog.run()
     dialog.destroy()
-def dl_audio(window, link, file_type, folder, artist, album):
+def dl_audio(window, link, file_type, folder, album = "", track_no = 0, track_total = 0, append_track_no = False):
         try:
             vid = YouTube(link)
         except:
@@ -72,5 +49,12 @@ def dl_audio(window, link, file_type, folder, artist, album):
         path_to_audio = path_to_vid[:-4] + file_type
         ffmpeg.input(path_to_vid).output(path_to_audio).run()
         if file_type == ".mp3":
-            system("eyeD3 -a '" + artist + "' -A '" + album + "' '" + path_to_audio + "'")
-        system("rm '" + path_to_vid + "'")
+                system('eyeD3 -a "' + vid.author.replace(" - Topic", "") + 
+                       '" -A "' + album.replace("Album - ", "") + 
+                       '" -t "' + vid.title + 
+                       '" -d ' + str(track_no) + 
+                       ' -D ' + str(track_total) + 
+                       ' "' + path_to_audio + '"')
+        system('rm "' + path_to_vid + '"')
+
+
--- index.py
+++ index.py
@@ -3,43 +3,33 @@ import gi
 from dl_handler import check_and_download
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
-args = {
-        "ft": ".mp3",
-        "is_album": True
-        } #Should have "link", "folder", "ft" (filetype) and "is_album". can also have "artist" and "album"
 builder = Gtk.Builder()
 builder.add_from_file("wow.glade")
 
 class Handlers:
+    filetype = None
+    link = None
+    folder = None
     """called when the x button is pressed"""
     def main_window_destroy_cb(self, _):
         Gtk.main_quit()
 
     """called when enter is pressed in the link input box"""
-    def link_activate_cb(self, link):
-        args["link"] = link.get_text()
+    def link_activate_cb(self, text):
+        self.link = text.get_text()
 
     """called when a folder is set in the dialogue"""
     def dir_choice_file_set_cb(self, file):
-        args["folder"] = file.get_filename()
-
-    def album_choice_toggled_cb(self, button):
-        args["is_album"] = button.get_active()
-
-    def artist_choice_activate_cb(self, artist):
-        args["artist"] = artist.get_text()
-
-    def album_choice_activate_cb(self, album):
-        args["album"] = album.get_text()
+        self.folder = file.get_filename()
 
     def filetype_choice_changed_cb(self, choice):
         choice_id = choice.get_active_id()
-        args["ft"] = ".mp3" if choice_id == "0" else ".wav"
+        self.filetype = ".mp3" if choice_id == "0" else ".wav"
     """called when the download button is pressed"""
     def download_clicked_cb(self, _):
         #TODO: create function in dl_handler.py to handle downloading all of the files
-        print("args:" + str(args))
-        download_jump(args)
+        print(f"link: {self.link}\nfolder: {self.folder}\nfiletype: {self.filetype}")
+        download_jump(self.link, self.folder, self.filetype)
 
     """called when the cancel button is pressed"""
     def cancel_clicked_cb(self, _):
@@ -48,6 +38,6 @@ class Handlers:
 builder.connect_signals(Handlers())
 window = builder.get_object("main_window")
 window.show_all()
-def download_jump(args):
-    check_and_download(window, args)
+def download_jump(link, folder, filetype):
+    check_and_download(window, link, folder, filetype)
 Gtk.main()
